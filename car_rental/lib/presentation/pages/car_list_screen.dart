1c0d5e8f9790599b4dac6ab63f13ccd29bd175e3
ElenaCV12 <148599044+ElenaCV12@users.noreply.github.com>
2025-04-28 21:56:24 -0600

    Bloc and Dependency injection

--- car_rental/lib/presentation/pages/car_list_screen.dart
+++ car_rental/lib/presentation/pages/car_list_screen.dart
@@ -1,13 +1,12 @@
 import 'package:car_rental/data/models/car.dart';
+import 'package:car_rental/presentation/bloc/car_bloc.dart';
+import 'package:car_rental/presentation/bloc/car_state.dart';
 import 'package:car_rental/presentation/widgets/car_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CarListScreen extends StatelessWidget {
-  final List<Car> cars = [
-    Car(model: 'Fortuner GR', distance: 870, fuelCapacity: 50, pricePerHour: 45),
-    Car(model: 'Fortuner GR', distance: 870, fuelCapacity: 50, pricePerHour: 45),
-    Car(model: 'Fortuner GR', distance: 870, fuelCapacity: 50, pricePerHour: 45),
-  ];
+  
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +16,27 @@ class CarListScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body: ListView.builder(
-        itemCount: cars.length,
-        itemBuilder: (context, index){
-          return CarCard(car: cars[index]);
+      body: BlocBuilder<CarBloc, CarState>(
+        builder: ( context, state){
+          if(state is CarsLoading){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          else if (state is CarsLoaded){
+            return ListView.builder(
+              itemCount: state.cars.length,
+              itemBuilder: (context, index){
+                return CarCard(car: state.cars[index]);
+              }
+            );
+          }
+                  else if (state is CarsError) {
+          return Center(child: Text('error: ${state.message}'),);
+        }
+        return Container();
         },
+
         ),
+
     );
   }
 }
